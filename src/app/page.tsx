3f0d50aa1f754c850b60cd2374db3830f4ceadfd
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-05-30 11:22:37 +0300

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import "./globals.css"
+import {Button} from "@/components/ui/button"
+import {Textarea} from "@/components/ui/textarea";
+import {Input} from "@/components/ui/input";
+import {Progress} from "@/components/ui/progress";
 
 export default function Home() {
-	return <div>
-		<p className="text-rose-700">
-			Hello, World!
-		</p>
+	return <div className="flex flex-col gap-y-4 p-4">
+		<div>
+			<Button variant="elevated">
+				I am a button
+			</Button>
+		</div>
+		<div>
+			<Input placeholder="What is your name?"/>
+		</div>
+		<div>
+			<Progress value={50}/>
+		</div>
+
+		<div>
+			<Textarea placeholder="What is your email address?"/>
+		</div>
+
 	</div>
 }
